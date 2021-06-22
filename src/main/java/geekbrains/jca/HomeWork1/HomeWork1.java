16946c1cf15a2bce0929769e96bbd40764faa749
unknown <RifatKholmat>
2021-06-22 13:09:10 +0500

    Initial commit

---
+++ src/main/java/geekbrains/jca/HomeWork1/HomeWork1.java
@@ -0,0 +1,43 @@
+package geekbrains.jca.HomeWork1;
+
+public class HomeWork1 {
+    /*public static void main (String[] args) {     // Упражнение 1
+        float a = 1.23f;
+        float b = 2.12f;
+        float c = 1.88f;
+        float d = 3.11f;
+        float result = a * (b + (c / d));
+        System.out.println(result);
+    }*/
+   /* public static void main (String[] args) {    // Упражнение 2
+        int a = 33;
+        int b = 2;
+            if (a + b < 10) {
+                System.out.println("false");
+            }
+            else if (a + b > 20) {
+                System.out.println("false");
+            }
+            else {
+                System.out.println("true");
+            }
+    }   */
+   /*public static void main (String[] args)  {   // Упражнение 3
+       int a = -5;
+       if (a >= 0) {
+           System.out.println("positive");
+       }
+       else {
+           System.out.println("negative");
+       }
+   }*/
+   /*public static void main (String[] args) {    // Упражнение 4
+       int a = -6;
+       boolean bool = a < 0;
+       System.out.println(bool);
+   }  */
+    public static void main (String[] args) {    //Упражнение 5
+        String name = "Вася";
+        System.out.println("Привет, " + name + "!");
+    }
+}
